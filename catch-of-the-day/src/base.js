17e1adee44998433760a51c9916c3dc6cc59443e
Sam Atkins <sam@cubi.io>
2017-03-12 16:09:47 +0000

    feat: Use Firebase to persist Fishes

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBlyLfQQ0zBQ_Vh96LVZV7lJr4Mdjenl8Q",
+  authDomain: "catch-of-the-day-160ae.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-160ae.firebaseio.com",
+});
+
+export default base;
